b72f731c14e504312d303213cacaf1d00a7c175c
Arshida <adrina.gashtasbi@gmail.com>
2024-08-08 11:18:12 +0330

    commit

--- 03.py
+++ 03.py
@@ -1,27 +1,27 @@
-from mpl_toolkits.basemap import Basemap
 import numpy as np
-from matplotlib import pyplot as plt
 import pandas as pd
-# create new figure, axes instances.
+from matplotlib import pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+
+lons = df["lon"]
+lats = df["lat"]
+sizes = df["population"]/10000
 fig=plt.figure()
 ax=fig.add_axes([0.1,0.1,0.8,0.8])
-# setup mercator map projection.
-m = Basemap(llcrnrlon=40.,llcrnrlat=20.,urcrnrlon=70.,urcrnrlat=40.,\
+
+m = Basemap(llcrnrlon=40.,llcrnrlat=24.,urcrnrlon=65.,urcrnrlat=40.,\
             rsphere=(6378137.00,6356752.3142),\
             resolution='l',projection='merc',\
             lat_0=10,lon_0=-20.,lat_ts=20.)
 m.drawcoastlines()
-m.fillcontinents()
+x , y =m(lons,lats)
 
 m.drawparallels(np.arange(10,90,20),labels=[1,1,0,1])
-
 m.drawmeridians(np.arange(-180,180,30),labels=[1,1,0,1])
-m.scatter(36,48)
-ax.set_title('Iran')
 
+df = pd.read_csv("ctr.csv")
 
-cities = pd.read_csv("ctr.csv")
+ax.set_title('most papulated city in iran')
 
-
-plt.scatter(cities["longitude"],cities["latitude"],s=cities["population"]/1000)
 plt.show()
